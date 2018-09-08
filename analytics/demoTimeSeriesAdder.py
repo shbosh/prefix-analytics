5fbde2c802fe6beef6c8e8922f45a5c146d42871
tessav <tessavoon@gmail.com>
2018-09-08 20:14:32 +0800

    model v1

--- analytics/demoTimeSeriesAdder.py
+++ analytics/demoTimeSeriesAdder.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
-import requests
 import ast
-import pickle
+import os
+import json
+import re
+import string
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction import text
-from sklearn.preprocessing import MultiLabelBinarizer
+from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.externals import joblib
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
@@ -18,6 +23,29 @@ from sklearn.metrics import accuracy_score
 class demoTimeSeriesAdder:
     def __init__(self):
         print "Create machine learning model"
+        self.stemmer = PorterStemmer()
+
+    def executeModel(self, data, modelmap={}):
+        # # TRAIN MODEL
+        # classifier, headers = self.train_model()
+        # # save classifier as pickle
+        # joblib.dump(classifier, 'model.pkl')
+        # with open('headers.txt', 'w') as file:
+        #     file.write(json.dumps(headers))
+        # # upload to blobstore
+
+        # RUN MODEL
+        data_json = json.loads(data)
+        #with open('analytics/headers.txt', 'r') as file: FIXME: FOR DEPLOYMENT
+        with open('headers.txt', 'r') as file:
+            headers = eval(file.read())
+        #classifier = joblib.load('analytics/model.pkl') FIXME: for deployment
+        classifier = joblib.load('model.pkl')
+        result = self.run_model(data_json['data'], classifier, headers)
+        return json.dumps({
+            'predicted': result[0]
+        })
+
 
     def add2NumberArrays(self, data, modelmap = {}):
         threshold = float(modelmap['threshold']) if len(modelmap) > 0 else None
@@ -51,7 +79,12 @@ class demoTimeSeriesAdder:
         svc_request_df, error_log_df = self.load_data_from_files()
         clean_sr_df = self.clean_svc_requests(svc_request_df)
         clean_el_df = self.clean_error_logs(error_log_df)
+        #clean_el_df = pd.read_csv('smaller_guy.csv')
         combined_df = self.combine_df(clean_sr_df, clean_el_df)
+        combined_df.to_csv('combined.csv')
+
+        with open('headers.txt', 'w') as file:
+            file.write(json.dumps(list(combined_df.columns.values)))
 
         features_train, features_test, labels_train, labels_test = train_test_split(combined_df,combined_df.Resolution_Code,test_size=0.2,random_state=1)
         ## Drop y from features
@@ -65,9 +98,7 @@ class demoTimeSeriesAdder:
         ## Accuracy
         rfc_predictions = classifier.predict(features_test)
         print accuracy_score(labels_test, rfc_predictions)
-        # save classifier as pickle
-        joblib.dump(classifier, 'model.pkl')
-        # upload to blobstore
+        return classifier, list(combined_df.columns.values)
 
 
     def load_data_from_files(self):
@@ -86,14 +117,8 @@ class demoTimeSeriesAdder:
         remove_words = r'\b(?:{})\b'.format('|'.join(remove_words))
 
         svc_request_df['symptom'] = svc_request_df['symptom'] \
-            .apply(lambda x: x.replace('[^\w\s]','') \
-                    .lower() \
-                    .replace(stopwords, '') \
-                    .replace('\d+', '') \
-                    .replace(remove_words, '') \
-            )
+            .apply(self.clean_symptom_text)
         # pivot words
-        # print svc_request_df.symptom
         vect = CountVectorizer()
         X = vect.fit_transform(svc_request_df['symptom'])
         count_vect_df = pd.DataFrame(X.todense(), columns=vect.get_feature_names())
@@ -103,6 +128,17 @@ class demoTimeSeriesAdder:
         ## merge with resolution_codes
         final_cleaned = pd.merge(final, svc_request_df, 'left', left_index=True, right_index=True)
         return final_cleaned
+
+    def clean_symptom_text(self, symptom):
+        symptom = re.sub(r'\w*\d\w*', '', symptom).strip().lower()
+        #symptom = [x for x in symptom if x.isalpha()]
+        tokens = nltk.word_tokenize(symptom)
+        remove_words = ['image:none-none-none', 'none-none-none']
+        stemmed_tokens = [self.stemmer.stem(token) or token for token in tokens if token not in remove_words]
+        final_tokens = set(stemmed_tokens) - set(stopwords.words('english'))
+        final_tokens_2 = final_tokens - set(string.punctuation)
+        # FIXME OTHER STEPS: SPELLING CHECKER & ISALPHA & REMOVE MORE WORDS & REGEX REMOVE IMAGE:XX
+        return ' '.join(list(final_tokens_2))
 
     def clean_error_logs(self, error_log_df):
         # remove non-numeric error codes
@@ -121,14 +157,25 @@ class demoTimeSeriesAdder:
         final_df = pd.merge(sr_df, el_df, 'left', on = 'sr_id').fillna(-999).drop('symptom_x', 1).drop('symptom_y', 1).drop('sr_id', 1).drop('Created_Date', 1)
         return final_df
 
-    def run_model(self, data):
-        classifier = joblib.load('model.pkl')
-        # create a df with all the features
-        #classifier.predict()
-
+    def run_model(self, data, classifier, headers):
+        tmp_dict = {}
+        for header in headers:
+            if isinstance(header, str) and header in self.clean_symptom_text(data['symptom']):
+                tmp_dict[header] = 1
+            elif isinstance(header, int) and header in data['error_codes']:
+                tmp_dict[header] = 1
+            else:
+                tmp_dict[header] = 0
+        df = pd.DataFrame([tmp_dict], columns=tmp_dict.keys()).drop('Resolution_Code', 1)
+        return classifier.predict(df)
 
 
 if __name__ == "__main__":
     demo = demoTimeSeriesAdder()
-    demo.train_model()
-    #demo.run_model()
+    print(demo.executeModel(json.dumps({
+            "data": {
+                    'sr_id': '123456',
+                    'error_codes': [],
+                    'symptom': 'Site power outage yesterday. UPS in bypass mode and battery breaker will not stay on.',
+                    }
+            }), ""))
